c3b64f2c96a0627474a7db4035158338ada880f2
Wesley Erasmus <wesley.erasmus119@gmail.com>
2023-09-17 20:08:34 +0200

    [Poll.js]: Added state to component and updated CSS

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,16 +1,15 @@
 /* shortcut for React template rafce
  -> short for  React arrow function component export
 */
-import React from "react";
+import React, { useState } from "react";
 import "../styles/poll.css";
 import VoteButton from "./VoteButton";
 import VoterImage from "./VoterImage";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-
-
-
 
 const Poll = () => {
+  // Initialize a state variable to track the selected programming language. Set to an empty string
+  const [selectedLanguage, setSelectedLanguage] = useState("");
+
   return (
     <div className="poll-container">
       {/* Polls Form */}
@@ -20,22 +19,39 @@ const Poll = () => {
             What programming language do you use during the coding interview?
           </div>
         </div>
+        {/* The checked attribute of each radio button is set based on whether the selectedLanguage matches the value of that radio button. When a radio button is selected, the onChange event triggers the corresponding setter function (setSelectedLanguage) to update the state with the new selected language. */}
         <div className="radio-buttons">
           <label>
-            <input type="radio" value="python" name="language" />
+            <input
+              type="radio"
+              value="python"
+              name="language"
+              checked={selectedLanguage === "python"}
+              // Update the selectedLanguage to python when this radio button is clicked
+              onChange={() => setSelectedLanguage("python")}
+            />
             Python
           </label>
           <label>
             <input
-              checked
               type="radio"
               value="javascript"
               name="language"
+              checked={selectedLanguage === "javascript"}
+              // Update the selectedLanguage to javascript when this radio button is clicked
+              onChange={() => setSelectedLanguage("javascript")}
             />
             JavaScript
           </label>
           <label>
-            <input type="radio" value="go" name="language" />
+            <input
+              type="radio"
+              value="go"
+              name="language"
+              checked={selectedLanguage === "go"}
+              // Update the selectedLanguage to go when this radio button is clicked
+              onChange={() => setSelectedLanguage("go")}
+            />
             Go
           </label>
         </div>
